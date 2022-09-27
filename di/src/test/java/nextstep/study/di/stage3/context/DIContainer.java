a917df83f25c6627be5a0fa9549ce48317ce73c9
yangdongjue5510 <djwhy5510@gmail.com>
2022-09-27 21:22:10 +0900

    stage 모두 통과하도록 구현

--- di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage3/context/DIContainer.java
@@ -1,6 +1,10 @@
 package nextstep.study.di.stage3.context;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +14,54 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = classes.stream()
+                .map(this::instantiate)
+                .collect(Collectors.toSet());
+        beans.forEach(this::init);
+    }
+
+    private Object instantiate(final Class<?> clazz) {
+        try {
+            final Constructor<?> constructor = clazz.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return constructor.newInstance();
+        } catch (NoSuchMethodException | InvocationTargetException | InstantiationException |
+                 IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void init(final Object instance) {
+        for (final Field field : instance.getClass().getDeclaredFields()) {
+            initDependency(instance, field);
+        }
+    }
+
+    private boolean containsBeanBy(final Class<?> type) {
+        return beans.stream().anyMatch(type::isInstance);
+    }
+
+    private void initDependency(final Object instance, final Field field) {
+        final Class<?> type = field.getType();
+        if (containsBeanBy(type)) {
+            setDependency(instance, field, getBean(type));
+        }
+    }
+
+    private void setDependency(final Object instance, final Field field, final Object dependency) {
+        try {
+            field.setAccessible(true);
+            field.set(instance, dependency);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(aClass::isInstance)
+                .findFirst()
+                .orElseThrow();
     }
 }
